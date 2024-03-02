3c0308705b67766c6fca57a8889ae7f5da6b9fad
niniack <nsa325@nyu.edu>
2024-03-02 11:43:40 +0400

    Fix parser

--- scripts/parse_toml.py
+++ scripts/parse_toml.py
@@ -3,7 +3,7 @@ import toml
 
 with open("training.toml", "r") as file:
     data = toml.load(file)
-    deps = data.get("dependencies", [])
+    deps = data.get("dependencies_list", [])
 
 with open("requirements.txt", "w") as f:
     for dep in deps:
